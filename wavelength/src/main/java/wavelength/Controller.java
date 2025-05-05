1a55e1a66f2854dac4e1d152edf36ef5bb2956be
LukeDaenzer <luzer557@gmail.com>
2025-05-04 20:53:50 -0500

    Basic window functionality

--- wavelength/src/main/java/wavelength/Controller.java
+++ wavelength/src/main/java/wavelength/Controller.java
@@ -1,6 +1,29 @@
 package wavelength;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
 public class Controller {
 
-    
+    @FXML
+    private Pane draggableWindow;
+
+    @FXML
+    private Label dragHandle;
+
+    private double mouseAnchorX;
+
+    @FXML
+    public void initialize(){
+        
+        dragHandle.setOnMousePressed(event -> {
+            mouseAnchorX = event.getSceneX() - draggableWindow.getTranslateX();
+        });
+
+        dragHandle.setOnMouseDragged(event -> {
+            double newTranslateX = event.getSceneX() - mouseAnchorX;
+            draggableWindow.setTranslateX(newTranslateX);
+        });
+    }
 }
